ff6e96fc01e02adb0469f77f7e76758a772b59d5
Beau Simensen <beau@dflydev.com>
2010-08-25 02:39:39 -0700

    o Add some additional information to the header.

--- functions.php
+++ functions.php
@@ -7,4 +7,15 @@ add_action( 'after_setup_theme', 'd2code_dflydev_setup' );
 function d2code_child_enqueue_style() {
     wp_enqueue_style('d2code_dflydev', get_bloginfo('stylesheet_directory').'/style.css', null, null, 'all');
 }
+
+function d2code_dflydev_print_styles() {
+    ?>
+    <link href="http://www.google.com/profiles/simensen" rel="me" type="text/html" />
+    <link href="http://www.google.com/profiles/srcmvn" rel="me" type="text/html" />
+    <link href="http://www.google.com/profiles/dflydev" rel="me" type="text/html" />
+    <link href="http://dflydev.com/favicon.ico" rel="shortcut icon" />
+    <meta name="google-site-verification" content="2s7zw_Jp175z5ry2upkaDlMUfhCGkUQfjqqYlEM_MpY" />
+    <?php
+}
+add_action('wp_print_styles', 'd2code_dflydev_print_styles');
 ?>
